724363a4af9ce1ebc29d3210a0d7ffa7915a5dc1
Andrea Andrea <andreacapoferri41@gmail.com>
2018-02-21 12:55:53 +0100

    TabLayout

--- app/src/main/java/com/smartmccg/homie/Home.java
+++ app/src/main/java/com/smartmccg/homie/Home.java
@@ -1,17 +1,52 @@
 package com.smartmccg.homie;
 
-import android.os.Bundle;
+
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 
-
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements Door.OnFragmentInteractionListener,Light.OnFragmentInteractionListener,Temperature.OnFragmentInteractionListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homepage);
+        setContentView(R.layout.home);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Temperature"));
+        tabLayout.addTab(tabLayout.newTab().setText("Light"));
+        tabLayout.addTab(tabLayout.newTab().setText("Door"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
-
-
