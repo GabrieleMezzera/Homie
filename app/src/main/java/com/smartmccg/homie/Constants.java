64849981197d4d77d6facffda1557dd37980a8bb
gabrielemezzera <gabrielemezzera@gmail.com>
2018-05-02 16:45:52 +0200

    Added MQTT Connection

---
+++ app/src/main/java/com/smartmccg/homie/Constants.java
@@ -0,0 +1,14 @@
+package com.smartmccg.homie;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by brijesh on 20/4/17.
+ */
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://m11.cloudmqtt.com:18451";
+
+}
+
--- app/src/main/java/com/smartmccg/homie/MainMenu.java
+++ app/src/main/java/com/smartmccg/homie/MainMenu.java
@@ -1,39 +1,43 @@
 package com.smartmccg.homie;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.text.Html;
 import android.util.Log;
 import android.view.SubMenu;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -47,7 +51,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainMenu extends AppCompatActivity {
-
+    PahoMqttClient mqttHelper;
+    MqttAndroidClient client;
     int LoadingStatus = 0;
     private DrawerLayout navigationMenuDrawer;
     private TextView userNameinNavDraw;
@@ -55,9 +60,10 @@ public class MainMenu extends AppCompatActivity {
     private String NavDrawHeaderUrl;
     private boolean UrlRecieved;
     private Bitmap NavDrawHeaderBitmap;
-    private boolean NavHeaderReady=false;
-    private boolean PlacesRecieved=false;
+    private boolean NavHeaderReady = false;
+    private boolean PlacesRecieved = false;
     private String ActualFragmentName;
+    public static Context appContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,8 +71,10 @@ public class MainMenu extends AppCompatActivity {
         ActualFragmentName = getResources().getString(R.string.summary);
         replaceFragment(new Summary());
         setContentView(R.layout.activity_main_menu);
+        appContext = getApplicationContext();
         SharedPreferences sp = getSharedPreferences("user_data", Activity.MODE_PRIVATE);
         String userName = sp.getString("userName", null);
+        HandleMqtt();
         new MainMenu.AsyncTopicRequest().execute(userName);
         LoadingTopicsListener();
         SetupthePage();
@@ -81,15 +89,14 @@ public class MainMenu extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
-        if (LoadingStatus==1) {
+        if (LoadingStatus == 1) {
             String FirstTopic = sp.getString("Description_1", null);
             sp.edit().putString("Description", sp.getString("Description_1", null)).commit();
             sp.edit().putString("Topic", sp.getString("Topic_1", null)).commit();
-            String HtmlReadyTitle = "<font color='" + String.format("#%06X", 0xFFFFFF & getResources().getColor(R.color.pureWhite)) + "'>"+ FirstTopic + "</font>";
+            String HtmlReadyTitle = "<font color='" + String.format("#%06X", 0xFFFFFF & getResources().getColor(R.color.pureWhite)) + "'>" + FirstTopic + "</font>";
             actionbar.setTitle(Html.fromHtml(HtmlReadyTitle));
-        }
-        else {
-            String HtmlReadyTitle = "<font color='" + String.format("#%06X", 0xFFFFFF & getResources().getColor(R.color.pureWhite)) + "'>"+ getString(R.string.app_name) + "</font>";
+        } else {
+            String HtmlReadyTitle = "<font color='" + String.format("#%06X", 0xFFFFFF & getResources().getColor(R.color.pureWhite)) + "'>" + getString(R.string.app_name) + "</font>";
             actionbar.setTitle(Html.fromHtml(HtmlReadyTitle));
         }
         new ImageUrlRequest().execute();
@@ -103,7 +110,7 @@ public class MainMenu extends AppCompatActivity {
     public void PreTopicChange(NavigationView navigationView) {
         navigationView.getMenu().clear();
         SharedPreferences sp = getSharedPreferences("about_topics", Activity.MODE_PRIVATE);
-        for (int i = 0; i<sp.getInt("topic_number", 1); i++) {
+        for (int i = 0; i < sp.getInt("topic_number", 1); i++) {
             String DescriptionKey = "Description_" + (i + 1);
             navigationView.getMenu().add(sp.getString(DescriptionKey, null)).setIcon(R.drawable.ic_map_pin_marked);
             TopicSelection = true;
@@ -111,22 +118,33 @@ public class MainMenu extends AppCompatActivity {
     }
 
     public void ManageTopicChange(MenuItem menuItem, NavigationView navigationView) {
+        SharedPreferences sp = getSharedPreferences("about_topics", Activity.MODE_PRIVATE);
+        try {
+            mqttHelper.unSubscribe(client, sp.getString("Topic", null));
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
         String SelectedDescription = menuItem.toString();
         String TopicSelected = null;
-        SharedPreferences sp = getSharedPreferences("about_topics", Activity.MODE_PRIVATE);
-        for (int i = 0; i<sp.getInt("topic_number", 1); i++) {
-            if(sp.getString("Description_" + (i+1), null).equals(SelectedDescription)) {
-                TopicSelected = sp.getString("Topic_" + (i+1), null);
+        for (int i = 0; i < sp.getInt("topic_number", 1); i++) {
+            if (sp.getString("Description_" + (i + 1), null).equals(SelectedDescription)) {
+                TopicSelected = sp.getString("Topic_" + (i + 1), null);
                 break;
             }
         }
         sp.edit().putString("Description", SelectedDescription).commit();
         sp.edit().putString("Topic", TopicSelected).commit();
-        Toast.makeText(getApplicationContext(), SelectedDescription +" "+ getString(R.string.selected), Toast.LENGTH_LONG).show();
-        String HtmlReadyTitle = "<font color='" + String.format("#%06X", 0xFFFFFF & getResources().getColor(R.color.pureWhite)) + "'>"+ sp.getString("Description", null) + "</font>";
+        try {
+            mqttHelper.subscribe(client, sp.getString("Topic", null), 1);
+        }
+        catch (MqttException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(getApplicationContext(), SelectedDescription + " " + getString(R.string.selected), Toast.LENGTH_LONG).show();
+        String HtmlReadyTitle = "<font color='" + String.format("#%06X", 0xFFFFFF & getResources().getColor(R.color.pureWhite)) + "'>" + sp.getString("Description", null) + "</font>";
         getSupportActionBar().setTitle(Html.fromHtml(HtmlReadyTitle));
         navigationMenuDrawer.closeDrawers();
-        TopicSelection=false;
+        TopicSelection = false;
     }
 
     public void NavigationDrawerClickHandler() {
@@ -136,7 +154,7 @@ public class MainMenu extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem){
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
                         // set item as selected to persist highlight
                         menuItem.setChecked(true);
 
@@ -145,11 +163,10 @@ public class MainMenu extends AppCompatActivity {
                             ManageTopicChange(menuItem, navigationView);
                         }
 
-                        if(menuItem.toString().equalsIgnoreCase(getString(R.string.change_topic))) {
+                        if (menuItem.toString().equalsIgnoreCase(getString(R.string.change_topic))) {
                             if (LoadingStatus == -3) {
                                 Toast.makeText(getApplicationContext(), getString(R.string.missing_topics), Toast.LENGTH_LONG).show();
-                            }
-                            else {
+                            } else {
                                 PreTopicChange(navigationView);
                             }
                         }
@@ -176,17 +193,15 @@ public class MainMenu extends AppCompatActivity {
                 userNameinNavDraw.setText(userinfo.getString("userName", null));
                 SharedPreferences sp = getSharedPreferences("about_topics", Activity.MODE_PRIVATE);
                 TextView selectedTopicinNavDraw = findViewById(R.id.nav_topicselected);
-                if (NavHeaderReady == true){
-                    linearLayout.setBackground(new BitmapDrawable(ThumbnailUtils.extractThumbnail(NavDrawHeaderBitmap,(NavDrawHeaderBitmap.getHeight()*linearLayout.getWidth()/linearLayout.getHeight()),NavDrawHeaderBitmap.getHeight())));
-                }
-                else {
+                if (NavHeaderReady == true) {
+                    linearLayout.setBackground(new BitmapDrawable(ThumbnailUtils.extractThumbnail(NavDrawHeaderBitmap, (NavDrawHeaderBitmap.getHeight() * linearLayout.getWidth() / linearLayout.getHeight()), NavDrawHeaderBitmap.getHeight())));
+                } else {
                     linearLayout.setBackground(new ColorDrawable(getResources().getColor(R.color.colorPrimary)));
                 }
                 if (LoadingStatus == -3) {
-                    selectedTopicinNavDraw.setText(getString(R.string.selectedTopic)+": " +getString(R.string.notopicavailable));
-                }
-                else {
-                    selectedTopicinNavDraw.setText(getString(R.string.selectedTopic)+": "+sp.getString("Topic", getString(R.string.notopicavailable)));
+                    selectedTopicinNavDraw.setText(getString(R.string.selectedTopic) + ": " + getString(R.string.notopicavailable));
+                } else {
+                    selectedTopicinNavDraw.setText(getString(R.string.selectedTopic) + ": " + sp.getString("Topic", getString(R.string.notopicavailable)));
                 }
 
                 return true;
@@ -195,33 +210,28 @@ public class MainMenu extends AppCompatActivity {
     }
 
     public void LoadingTopicsListener() {
-        while (LoadingStatus == 0) {}
+        while (LoadingStatus == 0) {
+        }
         SharedPreferences aboutTopics = getSharedPreferences("about_topics", Activity.MODE_PRIVATE);
         if (LoadingStatus == 1) {
-            Toast.makeText(getApplicationContext(), getString(R.string.loaded) +" " + aboutTopics.getInt("topic_number", -1) + " Topics", Toast.LENGTH_LONG).show();
-        }
-        else if (LoadingStatus == -1) {
+            Toast.makeText(getApplicationContext(), getString(R.string.loaded) + " " + aboutTopics.getInt("topic_number", -1) + " Topics", Toast.LENGTH_LONG).show();
+        } else if (LoadingStatus == -1) {
             Toast.makeText(getApplicationContext(), getString(R.string.connection_error), Toast.LENGTH_LONG).show();
-        }
-
-        else if (LoadingStatus == -2) {
+        } else if (LoadingStatus == -2) {
             Toast.makeText(getApplicationContext(), getString(R.string.Query_error), Toast.LENGTH_LONG).show();
-        }
-
-        else if (LoadingStatus == -3) {
+        } else if (LoadingStatus == -3) {
             Toast.makeText(getApplicationContext(), getString(R.string.missing_topics), Toast.LENGTH_LONG).show();
         }
     }
 
-    public void MakeNavDrawMenu (NavigationView NavView) {
+    public void MakeNavDrawMenu(NavigationView NavView) {
         Menu NavDrawMenu = NavView.getMenu();
         SharedPreferences sp = getSharedPreferences("about_topics", Activity.MODE_PRIVATE);
 
         SubMenu General = NavDrawMenu.addSubMenu(getResources().getString(R.string.general));
         if (ActualFragmentName.equals(getResources().getString(R.string.summary))) {
             General.add(getResources().getString(R.string.summary)).setIcon(R.drawable.ic_house).setChecked(true);
-        }
-        else {
+        } else {
             General.add(getResources().getString(R.string.summary)).setIcon(R.drawable.ic_house);
         }
         if (PlacesRecieved == true) {
@@ -240,6 +250,39 @@ public class MainMenu extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
+    public void HandleMqtt() {
+        SharedPreferences userinfo = getSharedPreferences("user_data", Activity.MODE_PRIVATE);
+        final SharedPreferences sp = getSharedPreferences( "about_topics", Activity.MODE_PRIVATE);
+        mqttHelper = new PahoMqttClient();
+        client = mqttHelper.getMqttClient(getApplicationContext(), Constants.MQTT_BROKER_URL, userinfo.getString("userName", null));
+        client.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                try {
+                    mqttHelper.subscribe(client, sp.getString("Topic", null), 1);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+                sp.edit().putString("LastMessage", new String (mqttMessage.getPayload())).commit();
+                Toast.makeText(getApplicationContext(), sp.getString("LastMessage", null), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+
     private class AsyncTopicRequest extends AsyncTask<String, String, Void> {
         ProgressDialog pdLoading = new ProgressDialog(MainMenu.this);
         HttpURLConnection conn;
@@ -255,6 +298,7 @@ public class MainMenu extends AppCompatActivity {
             pdLoading.show();
 
         }
+
         @Override
         protected Void doInBackground(String... params) {
             try {
@@ -325,34 +369,29 @@ public class MainMenu extends AppCompatActivity {
                         StoryTeller.commit();
                         if (finalRawMessage.equalsIgnoreCase("ERROR: Could not connect. ")) {
                             LoadingStatus = -1;
-                        }
-                        else if (finalRawMessage.equalsIgnoreCase("ERROR: Could not able to execute the Query")) {
+                        } else if (finalRawMessage.equalsIgnoreCase("ERROR: Could not able to execute the Query")) {
                             LoadingStatus = -2;
-                        }
-                        else if (finalRawMessage.equalsIgnoreCase("No records matching your query were found.")) {
+                        } else if (finalRawMessage.equalsIgnoreCase("No records matching your query were found.")) {
                             LoadingStatus = -3;
                         }
 
-                    }
-
-                    else {
+                    } else {
                         String[] splitTopics = finalRawMessage.split("ç");
                         SharedPreferences aboutTopics = getSharedPreferences("about_topics", Activity.MODE_PRIVATE);
                         SharedPreferences.Editor StoryTeller = aboutTopics.edit();
                         StoryTeller.putInt("topic_number", splitTopics.length);
-                        for (int i=0; i<splitTopics.length; i++) {
-                            String SplitString[]= splitTopics[i].split(String.valueOf("#"));
-                            String TopicKeyName = "Topic_" + (i+1);
+                        for (int i = 0; i < splitTopics.length; i++) {
+                            String SplitString[] = splitTopics[i].split(String.valueOf("#"));
+                            String TopicKeyName = "Topic_" + (i + 1);
                             StoryTeller.putString(TopicKeyName, SplitString[0]);
-                            String DescriptionKeyName = "Description_" + (i+1);
+                            String DescriptionKeyName = "Description_" + (i + 1);
                             StoryTeller.putString(DescriptionKeyName, SplitString[1]);
                         }
                         StoryTeller.commit();
                         LoadingStatus = 1;
                     }
 
-                }
-                else {
+                } else {
 
                     Toast.makeText(getApplicationContext(), getString(R.string.connection_error), Toast.LENGTH_LONG).show();
                 }
@@ -380,6 +419,7 @@ public class MainMenu extends AppCompatActivity {
             super.onPreExecute();
 
         }
+
         @Override
         protected Void doInBackground(String... params) {
             try {
@@ -463,9 +503,11 @@ public class MainMenu extends AppCompatActivity {
 
         protected void onPostExecute(Bitmap result) {
             NavDrawHeaderBitmap = result;
-            NavHeaderReady=true;
+            NavHeaderReady = true;
         }
     }
+
+
 
 
 }
